e237ec200e65d2d476f3ae187331bec9b1ee8fd8
delonyohan <delonyohan.s@gmail.com>
2025-12-14 19:40:29 +0700

    refactor: Move Streamlit app to app/streamlit_app.py and update devcontainer configuration

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -3,8 +3,12 @@ from PIL import Image
 import numpy as np
 import os
 import cv2
+import sys
 
-import app.model_loader as model_loader
+# Add the parent directory to the path to allow imports from `src`
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import model_loader
 from src.utils import AGE_CATEGORIES
 
 st.set_page_config(
